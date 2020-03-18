d9ac80e287bc1bae268c24912873dca1e45545e2
nestorrente <nestorrente@gmail.com>
2020-03-18 21:05:04 +0100

    Version 0.1.1 - Fix the missing files in "dist" folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 const BUNDLE_HEADER = `
-Reaction.js v0.1.0
+Reaction.js v0.1.1
 https://github.com/nestorrente/reactionjs
 
 Released under the MIT License.
